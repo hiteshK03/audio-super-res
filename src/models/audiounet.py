379e84972f189fd732cc5d70645e94ed4d746837
Hitesh Kandala <1603coderz@gmail.com>
2020-12-03 22:39:23 +0530

    Update audiounet.py

--- src/models/audiounet.py
+++ src/models/audiounet.py
@@ -61,7 +61,7 @@ class AudioUNet(Model):
           x = LeakyReLU(0.2)(x)
 
       # upsampling layers
-      for l, nf, fs, l_in in reversed(zip(range(L), n_filters, n_filtersizes, downsampling_l)):
+      for l, nf, fs, l_in in list(reversed(list(zip(range(L), n_filters, n_filtersizes, downsampling_l)))):
         with tf.name_scope('upsc_conv%d' % l):
           # (-1, n/2, 2f)
           x = (Convolution1D(nb_filter=2*nf, filter_length=fs, 
